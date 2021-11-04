d5f1df4d69c6e2085c0c1fbba6d1c557a2c856d8
Victor Farrell <jvictorfarrell@gmail.com>
2021-11-04 17:13:33 -0400

    quickfix prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  riotUrl: "https://na1.api.riotgames.com",
+  apikey: "?api_key=RGAPI-b767d3cb-8676-4e19-aa07-59d6f2c62980",
 };
